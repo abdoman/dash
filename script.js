babb42c121b3393606a32b4f3ca02e0e50f794ad
AIMOURAD <mourad@blooom.com>
2016-06-07 11:55:02 -0500

    better layout!

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var gridster;
+
 var main = function(){
 	$('#add-button').click(function(){
 			alert("hey");
 	});
 	
-	 gridster = $('.gridster ul').gridster({
+	 gridster = $('.gridster > ul').gridster({
 		widget_margins: [30,30],
 		widget_base_dimentions: [140,140],
-		min_cols: 3	,
-		resize: {enabled:true}
-		}).data(gridster);
+		
+		}).data('gridster');
 
 	 	var canvas = document.getElementById("mycanvas-line");
   		//canvas.width = $("#parent-to-can").width();
